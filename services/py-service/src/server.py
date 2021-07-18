a6d9fc39f65688b8f023328ca06032be7cd01408
Mauro Petrini <dev.mpetrini@gmail.com>
2021-07-17 23:20:34 -0300

    Add service folder and python flask api

---
+++ services/py-service/src/server.py
@@ -0,0 +1,50 @@
+"""
+Runs the service on your local machine.
+
+Usage:
+    ./server.py run [options]
+    ./server.py --help
+
+Options:
+    -h --help               Show this text.
+    --host <host>           Host that holds the server.
+    --port <port>           Port where the server listens for request.
+    --host-consul <host>    Host that holds consul server.
+    --port-consul <number>  Port where the consul server listens.
+"""
+
+from asyncio import sleep
+
+from flask import Flask
+from docopt import docopt
+import consul
+
+app = Flask(__name__)
+
+_opts = docopt(__doc__)
+
+_HOST = _opts['--host']
+_PORT = int(_opts['--port'])
+_HOST_CONSUL = _opts['--host-consul']
+_PORT_CONSUL = int(_opts['--port-consul'])
+
+
+c = consul.Consul(host=_HOST_CONSUL, port=_PORT_CONSUL)
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return 0
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            service = c.agent.service.register(name="py-service", address=_HOST, port=_PORT)
+            break
+        except (ConnectionError, consul.ConsulException) as e:
+            print(f'Reconnecting: {str(e)}')
+            sleep(0.5)
+
+    app.run(host=_HOST, port=_PORT)
+

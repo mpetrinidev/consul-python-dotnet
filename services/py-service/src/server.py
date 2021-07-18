b1d057f95db60cd6c67e330595e9a5bee32e6987
Mauro Petrini <dev.mpetrini@gmail.com>
2021-07-18 12:35:55 -0300

    Add health check endpoint to api

--- services/py-service/src/server.py
+++ services/py-service/src/server.py
@@ -15,7 +15,8 @@ Options:
 import uuid
 from asyncio import sleep
 
-from flask import Flask
+from consul import Check
+from flask import Flask, Response
 from docopt import docopt
 import consul
 
@@ -28,23 +29,34 @@ _PORT = int(_opts['--port'])
 _HOST_CONSUL = _opts['--host-consul']
 _PORT_CONSUL = int(_opts['--port-consul'])
 
-
 c = consul.Consul(host=_HOST_CONSUL, port=_PORT_CONSUL, dc="dc1")
 
 
 @app.route('/', methods=['GET'])
 def home():
-    return 0
+    return Response(status=200, response={'value': 'Ok'})
+
+
+@app.route('/health', methods=['GET'])
+def health():
+    # check dependencies
+    return Response(status=200)
 
 
 if __name__ == '__main__':
+    http_addr = f'http://{_HOST}:{_PORT}/health'
+    print(http_addr)
+
     while True:
         try:
-            c.agent.service.register(name="py-service", service_id=str(uuid.uuid4()), address=_HOST, port=_PORT)
+            c.agent.service.register(name="py-service",
+                                     service_id=str(uuid.uuid4()),
+                                     address=_HOST,
+                                     port=_PORT,
+                                     check=Check.http(http_addr, '1s', timeout='5s'))
             break
         except (ConnectionError, consul.ConsulException) as e:
             print(f'Reconnecting: {str(e)}')
             sleep(0.5)
 
     app.run(host=_HOST, port=_PORT)
-
